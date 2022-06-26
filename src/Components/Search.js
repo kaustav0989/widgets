cf2b7edddd9af3645985e1cce428cff08888c01f
Kaustav Paul <roni0989@gmail.com>
2022-06-27 00:09:12 +0530

    initial commit

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -9,7 +9,27 @@ const Search = () => {
     const [results,setResults] = useState([]);
     console.log(results);
 
+    const renderedResults = results.map( (result) => {
+        return (
+            <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    className="ui button">
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html : result.snippet }}></span>
+                </div>
+            </div>
+        );
+    });
+
     useEffect(() => {
+        if(term)
         //Async function definition and direct call 
         ( async () => {
             const response = await axios.get('https://en.wikipedia.org/w/api.php',{
@@ -41,6 +61,7 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </React.Fragment>
     );
 }
