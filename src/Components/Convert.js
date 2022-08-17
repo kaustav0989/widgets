b6deda2d267b8c2c5091839185c310214f475395
Kaustav Paul <roni0989@gmail.com>
2022-08-18 00:26:57 +0530

    little

--- src/Components/Convert.js
+++ src/Components/Convert.js
@@ -1,17 +1,25 @@
 import React,{useState,useEffect} from "react";
 import axios from "axios";
 
+
 const Convert = ( {language,text} ) => {
+    const [translated,setTranslated] = useState('');
     useEffect( () => {
-        axios.post('https://translation.googleapis.com/language/translate/v2', {} , {
-            params : {
-                q : text,
-                target : language.value,
-                key : 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-            }
-        })
-        console.log('Rerendered');
+        const doTranslation = async () => {
+            const response = await axios.post('https://translation.googleapis.com/language/translate/v2', {} , {
+                params : {
+                    q : text,
+                    target : language.value,
+                    key : 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                }
+            })
+            setTranslated(response.data.data.translations[0].translatedText);
+        };
+        doTranslation();
+        //console.log('Rerendered');
     },[language,text]);
+
+    return <div className="ui header">Output : {translated}</div>
 }
 
 export default Convert;
